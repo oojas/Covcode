a9603aac668bf15b292662d6f371fcdd961991e3
oojas <oojasgupta@gmail.com>
2021-04-29 09:06:24 +0530

    Pushed updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:covidresources/homeScreen.dart';
 import 'package:flutter/material.dart';
+import 'configure_web.dart' if (dart.library.html) 'configure_web.dart';
 
 void main() {
+  configureApp();
   runApp(new MaterialApp(
     home: mainPage(),
     debugShowCheckedModeBanner: false,
